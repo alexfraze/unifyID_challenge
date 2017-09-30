c87266ce7d9337f85985310ed795214d5711cc6a
alex frazier <afrazie1@andrew.cmu.edu>
2017-09-29 23:45:33 -0400

    added whitenoise wave creator

--- random_pull.py
+++ random_pull.py
@@ -1,7 +1,7 @@
 import urllib2
 from PIL import Image
-
-
+import struct
+import wave
 
 def pull_random_int(bottom,top,amount):
 	response = urllib2.urlopen("https://www.random.org/integers/?num={num}&min={min}&max={max}&col=1&base=10&format=plain&rnd=new".format(num=amount,min=bottom,max=top)).read()
@@ -18,5 +18,19 @@ def create_random_image():
 			pixels[i,j] = (randoms[i], randoms[j], 100)
 	img.show()
 
+def create_White_noise(SAMPLE_LEN):
+	#current method takes a conisderable amount of time
+	noise_output = wave.open('noise.wav', 'w')
+	noise_output.setparams((2, 2, 44100, 0, 'NONE', 'not compressed'))
+	values = pull_random_int(-32767, 32767, SAMPLE_LEN)
+	for i in range(0, SAMPLE_LEN):
+	        value = values[i]
+	        packed_value = struct.pack('h', value)
+	        noise_output.writeframes(packed_value)
+	        noise_output.writeframes(packed_value)
+
+	noise_output.close()
+
 
 create_random_image()
+create_White_noise(10000) #132,300 is 3 seconds
